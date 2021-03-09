970c94c397ea3dc039582a4b3188abd87d63cea9
Mremmalex <mremmalex2@gmail.com>
2021-03-09 21:37:32 +0100

    learning java

--- main/Forloop.java
+++ main/Forloop.java
@@ -3,9 +3,23 @@ package main;
 
 public class Forloop {
     
-   public static void main(String[] args) {
-       for (int i = 0; i < 5; i++) {
-        System.out.println(i);
-       }
-   } 
+    public static void main(String[] args) {
+        // for loop 
+        // for (int i = 0; i < 5; i++) {
+        //  System.out.println(i);
+        // }
+        //looping over an array 
+
+        String[] cars = { "bmw", "volvo", "ford", "mazda" };
+
+        for (String i : cars) {
+           System.out.println(i); 
+        }
+
+        for (int i = 0; i < cars.length; i++){
+            System.out.println(cars[i]);
+        }
+
+    }
+
 }
